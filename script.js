26cdedc3fbaf2f6e4657dc54c0f8a11b06bdbd19
kgdee <kgdee7603@gmail.com>
2025-06-07 17:27:42 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,244 @@
+const projectName = "card-manager";
+const itemContainer = document.querySelector(".item-container");
+const searchBox = document.querySelector(".search-box");
+const breadcrumbs = document.querySelector(".breadcrumbs");
+const editModeBtn = document.querySelector(".edit-mode");
+
+const rootFolder = { id: null, name: "Root", path: [] };
+let currentItems = JSON.parse(localStorage.getItem(`${projectName}_items`)) || [];
+let currentFolder = rootFolder;
+let editMode = true;
+
+let darkTheme = JSON.parse(localStorage.getItem(`${projectName}_darkTheme`)) || false;
+
+document.addEventListener("DOMContentLoaded", function () {
+  displayItems();
+  editModeBtn.classList.toggle("active", editMode);
+  displayBreadcrumbs();
+  toggleTheme(darkTheme);
+});
+
+window.addEventListener("error", (event) => {
+  const error = `${event.type}: ${event.message}`;
+  console.error(error);
+  alert(error);
+});
+
+function stopPropagation(event) {
+  event.stopPropagation();
+}
+
+function getFileContent(file) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onload = (event) => resolve(event.target.result);
+    reader.onerror = (error) => reject(error);
+    reader.readAsDataURL(file);
+  });
+}
+
+function getItem(itemId) {
+  return currentItems.find((item) => item.id === itemId);
+}
+
+function createItem(item) {
+  if (!item.name) return;
+
+  currentItems.push(item);
+  localStorage.setItem(`${projectName}_items`, JSON.stringify(currentItems));
+
+  displayItems();
+  ItemModal.toggle();
+  Toast.show("Item has been successfully added");
+}
+
+function updateItem(index, updates) {
+  if (!updates.name) return;
+
+  let item = currentItems[index];
+
+  const updatedItem = { ...item, ...updates };
+  currentItems[index] = updatedItem;
+
+  localStorage.setItem(`${projectName}_items`, JSON.stringify(currentItems));
+  displayItems();
+  Toast.show("Item has been successfully updated");
+}
+
+function deleteItem(index) {
+  currentItems.splice(index, 1);
+  localStorage.setItem(`${projectName}_items`, JSON.stringify(currentItems));
+  displayItems();
+  Toast.show("Item has been successfully deleted");
+}
+
+function displayItems(items) {
+  if (!items) items = currentItems.filter((item) => item.parentId === currentFolder.id);
+  itemContainer.innerHTML =
+    items
+      .map(
+        (item) =>
+          `
+      <div class="item ${item.type}" onclick="handleItem('${item.id}')">
+        <img src="${item.icon}">
+        <p class="item-text truncated">${item.name}</p>
+      </div>
+    `
+      )
+      .join("") || "No items found";
+}
+
+function handleItem(itemId) {
+  const item = getItem(itemId);
+  if (editMode) {
+    ItemModal.toggle(item.id);
+  } else {
+    item.type === "card" ? DetailsModal.toggle(item.id) : openFolder(item.id);
+  }
+}
+
+function openFolder(ref) {
+  if (Number.isFinite(ref)) {
+    currentFolder = currentItems[ref];
+  } else if (typeof ref === "string") {
+    currentFolder = currentItems.filter((item) => item.id === ref)[0];
+  }
+  if (!currentFolder) currentFolder = rootFolder;
+  displayItems();
+  displayBreadcrumbs();
+}
+
+const ItemModal = (() => {
+  const element = document.querySelector(".item-modal");
+  const title = element.querySelector(".title");
+  const nameInput = element.querySelector(".name-input");
+  const descriptionInput = element.querySelector(".description-input");
+  const iconInput = element.querySelector(".icon-input");
+  const iconFileInput = iconInput.querySelector("input[type=file]");
+  const submitBtn = element.querySelector(".submit");
+  const deleteBtn = element.querySelector(".delete");
+  let currentItemType = "card";
+
+  function generateItem() {
+    const item = {
+      id: crypto.randomUUID(),
+      name: nameInput.value,
+      type: currentItemType,
+      parentId: currentFolder.id,
+      path: [...currentFolder.path, { id: currentFolder.id, name: currentFolder.name }],
+      description: descriptionInput.value,
+      icon: iconFileInput.value || "icon-default.png",
+    };
+    return item;
+  }
+
+  function toggle(itemId, itemType) {
+    const item = getItem(itemId);
+    currentItemType = itemType || (item ? item.type : "card");
+    iconFileInput.value = "";
+    title.textContent = `Create a new ${currentItemType}`;
+    submitBtn.textContent = "Create";
+    submitBtn.onclick = () => createItem(generateItem());
+    deleteBtn.classList.toggle("hidden", true);
+    iconInput.style.backgroundImage = null
+    nameInput.value = "";
+    descriptionInput.value = "";
+
+    if (item) {
+      title.textContent = `Update ${item.name}`;
+      submitBtn.textContent = "Update";
+      deleteBtn.classList.toggle("hidden", false);
+      const itemIndex = currentItems.indexOf(item);
+      submitBtn.onclick = () => updateItem(itemIndex, generateItem());
+      deleteBtn.onclick = () => deleteItem(itemIndex);
+
+      iconInput.style.backgroundImage = `url(${item.icon})`
+
+      nameInput.value = item.name || "";
+      descriptionInput.value = item.description || "";
+    }
+    element.classList.toggle("hidden");
+  }
+
+  return { toggle };
+})();
+
+const DetailsModal = (() => {
+  const element = document.querySelector(".details-modal");
+  const imageEl = element.querySelector(".image");
+  const nameEl = element.querySelector(".name");
+  const descriptionEl = element.querySelector(".description");
+
+  function toggle(itemId) {
+    const item = getItem(itemId);
+    element.classList.toggle("hidden");
+    imageEl.src = "";
+    nameEl.textContent = "Item name";
+    descriptionEl.textContent = "Item description";
+
+    if (item) {
+      imageEl.src = item.icon;
+      nameEl.textContent = item.name;
+      descriptionEl.textContent = item.description;
+    }
+  }
+
+  return { toggle };
+})();
+
+function toggleTheme(force = undefined) {
+  const toggle = document.querySelector(".theme-toggle");
+  force === undefined ? (darkTheme = !darkTheme) : (darkTheme = force);
+  localStorage.setItem(`${projectName}_darkTheme`, darkTheme);
+  document.body.classList.toggle("dark-theme", darkTheme);
+  toggle.innerHTML = darkTheme ? `<i class="bi bi-sun"></i>` : `<i class="bi bi-moon"></i>`;
+}
+
+const Toast = (() => {
+  const container = document.querySelector(".toast-container");
+  let itemCount = 0;
+
+  function show(message) {
+    if (!message) return;
+    const index = itemCount;
+    container.innerHTML += `
+      <div class="toast" data-toast="${index}">
+        ${message}
+      </div>
+    `;
+
+    setTimeout(() => {
+      const item = container.querySelector(`[data-toast="${index}"]`);
+      item.remove();
+    }, 3000);
+
+    itemCount++;
+  }
+
+  return { show };
+})();
+
+function search(terms) {
+  const items = currentItems.filter((item) => item.parentId === currentFolder.id);
+  const query = terms.trim().toLowerCase();
+  const filteredItems = query ? items.filter((item) => item.name.toLowerCase().includes(query)) : null;
+  displayItems(filteredItems);
+}
+
+function toggleEditMode() {
+  editMode = !editMode;
+  editModeBtn.classList.toggle("active", editMode);
+}
+
+function displayBreadcrumbs() {
+  breadcrumbs.innerHTML = "";
+  if (currentFolder?.path)
+    breadcrumbs.innerHTML += currentFolder.path
+      .map(
+        (route) => `
+            <button onclick="openFolder('${route.id}')">${route.name}</button> /
+          `
+      )
+      .join("");
+  breadcrumbs.innerHTML += currentFolder.name;
+}
